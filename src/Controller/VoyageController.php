eac55fba8496ec5c66bbd6171e1861b6926bec65
alpha <alpha@DESKTOP-AAOCKAT>
2022-02-05 15:59:03 +0100

    Phase 4 : Plusieurs pages

--- src/Controller/VoyageController.php
+++ src/Controller/VoyageController.php
@@ -7,18 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Description of AccueilController
+ * Description of VoyageController
  *
  * @author alpha
  */
-class AccueilController extends AbstractController {
-
+class VoyageController extends AbstractController {
+    
     /**
-     * @Route("/", name="accueil")
+     * @Route("/voyages", name="voyages")
      * @return Response
      */
     public function index(): Response{
-        return $this->render("pages/accueil.html.twig");
+        return $this->render("pages/voyages.html.twig");
     }
     
 }
