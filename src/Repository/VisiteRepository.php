024ca61d64c1dd0fc2d217e8350e799ec109b8d9
alpha <alpha@DESKTOP-AAOCKAT>
2022-02-09 14:08:23 +0100

    Phase 10 : ORM (filtrage des enregistrements).

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -31,6 +31,28 @@ class VisiteRepository extends ServiceEntityRepository
                 ->getQuery()
                 ->getResult();
     }
+    
+    /**
+     * 
+     * @param type $champ
+     * @param type $valeur
+     * @return Visite[]
+     */
+    public function findByEqualValue($champ, $valeur) : array {
+        if($valeur==""){
+            return $this->createQueryBuilder('v') //alias de la table
+                    ->orderBy('v.' .$champ, 'ASC')
+                    ->getQuery()
+                    ->getResult();
+        }else{
+            return $this->createQueryBuilder('v') //alias de la table
+                    ->where('v.'.$champ.'=:valeur')
+                    ->setParameter('valeur', $valeur)
+                    ->orderBy('v.datecreation','DESC')
+                    ->getQuery()
+                    ->getResult();
+        }
+    }
     // /**
     //  * @return Visite[] Returns an array of Visite objects
     //  */
